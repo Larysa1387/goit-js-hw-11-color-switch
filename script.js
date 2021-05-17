c3fc50bbc6c416c9fd6588c5c05d1aa6b76d680f
Larysa1387 <kaliuzhna.off@gmail.com>
2021-05-17 10:58:26 +0300

    task done

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const refs = {
+  btnStart: document.querySelector('[data-action="start"]'),
+  btnEnd: document.querySelector('[data-action="stop"]'),
+  body: document.body,
+}
+
+const DELAY = 1000;
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', onColorChange);
+
+function onColorChange(e) {
+  e.target.disabled = true;
+  intervalId = setInterval(() => {
+    const index = randomIntegerFromInterval(0, colors.length - 1);
+    refs.body.style.background = colors[index];
+  }, DELAY)
+}
+
+refs.btnEnd.addEventListener('click', onColorStop);
+
+function onColorStop() {
+  refs.btnStart.disabled = false;
+  clearInterval(intervalId);
+}
